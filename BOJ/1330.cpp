8638fd21c8e99e68e8eb5ebe04a3a425076e3f39
qmdl980 <qmdl98056@gmail.com>
2021-04-16 22:52:06 +0900

    210416 BOJ 2

---
+++ BOJ/1330.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int num, num1;
+	cin >> num >> num1;
+	if (num > num1) {
+		cout << ">";
+	}
+	else if (num < num1) {
+		cout << "<";
+	}
+	else if (num == num1) {
+		cout << "==";
+	}
+}
