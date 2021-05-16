8ccc6b21afa75f7d7cd176a43215dac5175f38a6
qmdl980 <qmdl98056@gmail.com>
2021-05-16 13:23:02 +0900

    210516 BOJ 15657

---
+++ BOJ/15657.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+bool visit[8]{ false };
+vector<int> inp;
+int ans[8];
+int cnt = 0;
+
+
+void input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		inp.push_back(num);
+	}
+	sort(inp.begin(), inp.end());
+}
+
+void solution(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (depth == 0) {
+			ans[depth] = inp[i];
+			solution(depth + 1);
+		}
+		else if (depth != 0 && ans[depth - 1] <= inp[i]) {
+			ans[depth] = inp[i];
+			solution(depth + 1);
+		}
+			
+	}
+}
+
+int main() {
+	input();
+	solution(0);
+}
