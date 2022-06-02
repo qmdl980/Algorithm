dc44e76a461ba68046e1dfa7beab677dd0aa0006
HYUKJIN OH <qmdl98056@gmail.com>
2022-06-02 21:03:59 +0900

    220602 10826 피보나치 수 4

---
+++ BOJ/10826.py
@@ -0,0 +1,20 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+n = int(input())
+if n == 0:
+    print(0)
+    exit(0)
+elif n < 3:
+    print(1)
+    exit(0)
+dp = [0] * (n + 1)
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[n])
