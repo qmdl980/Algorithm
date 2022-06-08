019fd8e6dd2a3041e854f6f3e5798cdfb08a3223
HYUKJIN OH <qmdl98056@gmail.com>
2022-06-08 22:47:18 +0900

    220608 1927 최소 힙

---
+++ BOJ/1927.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+n = int(input())
+heap = []
+
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        if len(heap):
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, num)
